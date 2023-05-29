0fe30b4cd32128f730286dbc28fa57f5846b4679
Patriciaaa <patricia.c2005@aluno.ifsc.edu.br>
2023-05-29 13:23:33 -0300

    INSERT ENDERECOS

--- scripts.sql
+++ scripts.sql
@@ -1,4 +1,4 @@
--- DROP DATABASE get_that_auto IF EXISTS;
+ DROP DATABASE  IF EXISTS get_that_auto ;
 
 CREATE DATABASE IF NOT EXISTS get_that_auto;
 
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS `clientes` (
   `email` VARCHAR(45) NOT NULL,
   `enderecos_cep` INT NOT NULL,
   PRIMARY KEY (`pessoas_cpf`),
-  CONSTRAINT `fk_clientes_enderecos1` FOREIGN KEY (`enderecos_cep`) REFERENCES.`enderecos` (`cep`)
+  CONSTRAINT `fk_clientes_enderecos1` FOREIGN KEY (`enderecos_cep`) REFERENCES `enderecos` (`cep`)
 );
 
 -- -----------------------------------------------------
@@ -59,8 +59,8 @@ CREATE TABLE IF NOT EXISTS `vendaRegistros` (
     `vendedores_idvendedores`,
     `clientes_pessoas_cpf`
   ),
-  CONSTRAINT `fk_vendaRegistros_vendedores1` FOREIGN KEY (`vendedores_idvendedores`) REFERENCES.`vendedores` (`idvendedores`),
-  CONSTRAINT `fk_vendaRegistros_clientes1` FOREIGN KEY (`clientes_pessoas_cpf`) REFERENCES.`clientes` (`pessoas_cpf`)
+  CONSTRAINT `fk_vendaRegistros_vendedores1` FOREIGN KEY (`vendedores_idvendedores`) REFERENCES `vendedores` (`idvendedores`),
+  CONSTRAINT `fk_vendaRegistros_clientes1` FOREIGN KEY (`clientes_pessoas_cpf`) REFERENCES `clientes` (`pessoas_cpf`)
 );
 
 -- -----------------------------------------------------
@@ -74,7 +74,7 @@ CREATE TABLE IF NOT EXISTS `fornecedores` (
   `telefone` INT NOT NULL,
   `fornecedorescol` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`cnpj`),
-  CONSTRAINT `fk_fornecedores_enderecos` FOREIGN KEY (`enderecos_cep`) REFERENCES.`enderecos` (`cep`)
+  CONSTRAINT `fk_fornecedores_enderecos` FOREIGN KEY (`enderecos_cep`) REFERENCES `enderecos` (`cep`)
 );
 
 -- -----------------------------------------------------
@@ -101,8 +101,8 @@ CREATE TABLE IF NOT EXISTS `produtos` (
   `categorias_idcategorias` INT NOT NULL,
   `fornecedores_cnpj` INT NOT NULL,
   PRIMARY KEY (`idprodutos`, `fornecedores_cnpj`),
-  CONSTRAINT `fk_produtos_categorias1` FOREIGN KEY (`categorias_idcategorias`) REFERENCES.`categorias` (`idcategorias`),
-  CONSTRAINT `fk_produtos_fornecedores1` FOREIGN KEY (`fornecedores_cnpj`) REFERENCES.`fornecedores` (`cnpj`)
+  CONSTRAINT `fk_produtos_categorias1` FOREIGN KEY (`categorias_idcategorias`) REFERENCES `categorias` (`idcategorias`),
+  CONSTRAINT `fk_produtos_fornecedores1` FOREIGN KEY (`fornecedores_cnpj`) REFERENCES `fornecedores` (`cnpj`)
 );
 
 -- -----------------------------------------------------
@@ -117,9 +117,35 @@ CREATE TABLE IF NOT EXISTS `carrinhos` (
     `vendaRegistros_idvenda`,
     `vendaRegistros_vendedores_idvendedores`
   ),
-  CONSTRAINT `fk_produtos_has_vendaRegistros_produtos1` FOREIGN KEY (`produtos_idprodutos`) REFERENCES.`produtos` (`idprodutos`),
+  CONSTRAINT `fk_produtos_has_vendaRegistros_produtos1` FOREIGN KEY (`produtos_idprodutos`) REFERENCES `produtos` (`idprodutos`),
   CONSTRAINT `fk_produtos_has_vendaRegistros_vendaRegistros1` FOREIGN KEY (
     `vendaRegistros_idvenda`,
     `vendaRegistros_vendedores_idvendedores`
-  ) REFERENCES.`vendaRegistros` (`idvenda`, `vendedores_idvendedores`)
+  ) REFERENCES `vendaRegistros` (`idvenda`, `vendedores_idvendedores`)
 );
+
+insert into enderecos (cep, rua, bairro, cidade, estado) values ('8595', 'Holmberg', 'Fullstone', '611 Parkside Center', 'Autumn Leaf');
+insert into enderecos (cep, rua, bairro, cidade, estado) values ('1973', 'Oak Valley', 'MacGowing', '638 Myrtle Road', 'Service');
+insert into enderecos (cep, rua, bairro, cidade, estado) values ('363', 'Prairieview', 'Mussared', '4483 Artisan Avenue', 'Eagle Crest');
+insert into enderecos (cep, rua, bairro, cidade, estado) values ('59724', 'Merchant', 'Korpal', '4489 Village Green Avenue', 'Alpine');
+insert into enderecos (cep, rua, bairro, cidade, estado) values ('5', 'Nevada', 'Sharma', '173 Stang Park', 'Hintze');
+insert into enderecos (cep, rua, bairro, cidade, estado) values ('58', 'Steensland', 'Egre', '90395 Maywood Court', 'Atwood');
+insert into enderecos (cep, rua, bairro, cidade, estado) values ('9450', 'Pepper Wood', 'Sikora', '0838 Carey Center', 'Westport');
+insert into enderecos (cep, rua, bairro, cidade, estado) values ('1641', 'Vera', 'Plunket', '677 Esch Park', 'Shopko');
+insert into enderecos (cep, rua, bairro, cidade, estado) values ('63', 'Spohn', 'Stigers', '1 Walton Lane', 'Mifflin');
+insert into enderecos (cep, rua, bairro, cidade, estado) values ('24402', 'Calypso', 'Cake', '53172 Moulton Crossing', 'Cottonwood');
+insert into enderecos (cep, rua, bairro, cidade, estado) values ('5423', 'Dwight', 'Vaudre', '06385 Prairie Rose Terrace', 'Kings');
+insert into enderecos (cep, rua, bairro, cidade, estado) values ('26', 'Dwight', 'Causer', '15 Vidon Junction', 'Hansons');
+insert into enderecos (cep, rua, bairro, cidade, estado) values ('58597', 'Sauthoff', 'Fulleylove', '0 Old Gate Plaza', 'Farmco');
+insert into enderecos (cep, rua, bairro, cidade, estado) values ('40', 'Southridge', 'Sharman', '398 Pleasure Street', 'Delaware');
+insert into enderecos (cep, rua, bairro, cidade, estado) values ('407', 'Mcguire', 'Tampen', '78367 Susan Point', 'Birchwood');
+insert into enderecos (cep, rua, bairro, cidade, estado) values ('86394', 'Dorton', 'Whitwham', '592 Logan Pass', 'Sachtjen');
+insert into enderecos (cep, rua, bairro, cidade, estado) values ('2343', 'Lakeland', 'Longhorn', '208 Nobel Avenue', 'Norway Maple');
+insert into enderecos (cep, rua, bairro, cidade, estado) values ('72', 'Beilfuss', 'Enoksson', '315 Brown Road', 'Ilene');
+insert into enderecos (cep, rua, bairro, cidade, estado) values ('413', 'Talmadge', 'Winckle', '32 Farragut Pass', 'Cottonwood');
+insert into enderecos (cep, rua, bairro, cidade, estado) values ('401', 'Holmberg', 'Jutson', '932 Longview Crossing', 'Starling');
+insert into enderecos (cep, rua, bairro, cidade, estado) values ('41', 'Dixon', 'Brogi', '665 John Wall Alley', 'Carioca');
+insert into enderecos (cep, rua, bairro, cidade, estado) values ('16', 'Golf', 'O''Docherty', '01 Muir Hill', 'Logan');
+insert into enderecos (cep, rua, bairro, cidade, estado) values ('93', 'Kinsman', 'Shorrock', '424 Mallard Center', 'Warner');
+insert into enderecos (cep, rua, bairro, cidade, estado) values ('312', 'Dwight', 'Brewitt', '85 Muir Alley', 'Huxley');
+insert into enderecos (cep, rua, bairro, cidade, estado) values ('14', 'Fieldstone', 'Buckney', '9 Dayton Plaza', 'Waubesa');
